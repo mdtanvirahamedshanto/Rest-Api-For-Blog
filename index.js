465d2ecfea27f694b74818b2d166e4412b37c243
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2023-12-07 01:21:51 +0600

    new

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-// const PORT = 3000;
+
+
+
+app.get('/', (req, res )=>{
+    res.send("hello world");
+})
+
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`); 
 });
