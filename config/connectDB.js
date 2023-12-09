f83763bf42f06bf4181be0697020c95c5f26081d
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2023-12-09 19:27:06 +0600

    login With JWT complete

--- config/connectDB.js
+++ config/connectDB.js
@@ -7,6 +7,7 @@ const connectDB = async ()=>{
 
     } catch (error) {
         console.log(error);
+        console.log("Database Not Connect");
     }
 }
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+// Sing Up Function
 exports.singup = async (req, res) => {
   try {
     req.body.password = await bcrypt.hash(req.body.password, 10);
@@ -22,6 +24,34 @@ exports.singup = async (req, res) => {
   }
 };
 
+// Login Function
 exports.loging = async (req, res) => {
-  res.send("controller login");
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({
+        massage: "Wrong Cradential !",
+      });
+    }
+    const validate = await bcrypt.compare(password, user.password);
+
+    if (!validate) {
+      return res.status(400).json({
+        massage: "Password Dosen't Match! ",
+      });
+    }
+    const token = await jwt.sign(
+      { username, _id: user._id },
+      process.env.PRIVATE_KEY,
+      { expiresIn: "2h" }
+    );
+
+    res.status(201).json({
+      massage: "Login SuccessFully! 🫡",
+      token
+    });
+  } catch (error) {
+    console.log("Something Went Wrong !");
+  }
 };
