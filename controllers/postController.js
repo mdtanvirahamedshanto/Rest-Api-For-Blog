08ad0a4c9a7a7ec6e40a67a3138edb4891274bd7
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2023-12-12 20:15:41 +0600

    complete post

--- controllers/postController.js
+++ controllers/postController.js
@@ -26,10 +26,81 @@ exports.createPost = async (req, res) => {
 //    Get All Post
 
 exports.getAllPost = async (req, res) => {
+  const { username, catagory } = req.query;
   try {
-    const posts = await Post.find();
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catagory) {
+      posts = await Post.find({
+        catagory: {
+          $in: [catagory],
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
     res.status(200).json({
       posts,
+    });
+  } catch (error) {
+    res.status(401).json({
+      massage: "Something went wrong! ",
+    });
+  }
+};
+
+//     Update post
+
+exports.updatePost = async (req, res) => {
+  const postId = req.headers.params;
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status("Not Find post");
+    }
+    const updatepost = await Post.findByIdAndUpdate(postId, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      massage: "Update your post successfully! ",
+      updatepost,
+    });
+  } catch (error) {
+    res.status(401).json({
+      massage: "You can update only your post ",
+    });
+  }
+};
+
+//    Delete post
+
+exports.deletePost = async (req, res) => {
+  const postId = req.headers.params;
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status("Not Find post");
+    }
+    await Post.findByIdAndDelete(postId);
+    res.status(200).json({
+      massage: "Delete your post successfully! ",
+    });
+  } catch (error) {
+    res.status(401).json({
+      massage: "You can delete only your post ",
+    });
+  }
+};
+
+//    get single post
+
+exports.getPost = async (req, res) => {
+  const postId = req.headers.params;
+  try {
+    const post = await Post.findById(postId)
+    res.status(200).json({
+      post,
     });
   } catch (error) {
     res.status(401).json({
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,9 +1,12 @@
-const { createPost, getAllPost } = require("../controllers/postController");
+const { createPost, getAllPost, updatePost, deletePost, getPost } = require("../controllers/postController");
 const { authMiddleWare } = require("../middlewares/auth");
 
 const postRoute = require("express").Router();
 
 postRoute.post("/post", authMiddleWare, createPost);
-postRoute.get("/posts", authMiddleWare, getAllPost);
+postRoute.get("/posts", getAllPost);
+postRoute.get("/post", getPost);
+postRoute.put("/update:postId", authMiddleWare, updatePost);
+postRoute.delete("/delete:postId", authMiddleWare, deletePost);
 
 module.exports = postRoute;
